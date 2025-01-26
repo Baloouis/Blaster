4a333db19a05b9ddcdc6ec1157de670c6f8c9078
Damien <48656735+Baloouis@users.noreply.github.com>
2025-01-26 23:33:15 +0100

    feat: setting up blasterCharacter input management with the enhanced input system

---
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlasterCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+ABlasterCharacter::ABlasterCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetMesh());
+	CameraBoom->TargetArmLength = 600.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+	
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+}
+
+void ABlasterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABlasterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(CharacterMappingContext, 0);
+		}
+	}
+}
+
+void ABlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		//Moving
+		EnhancedInputComponent->BindAction(MoveForwardAction, ETriggerEvent::Triggered, this, &ThisClass::MoveForward);
+		EnhancedInputComponent->BindAction(MoveRightAction, ETriggerEvent::Triggered, this, &ThisClass::MoveRight);
+
+		//Rotation
+		EnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered, this, &ThisClass::Turn);
+		EnhancedInputComponent->BindAction(LookUpAction, ETriggerEvent::Triggered, this, &ThisClass::LookUp);
+
+		//Jumping
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+
+	}
+	
+}
+
+void ABlasterCharacter::MoveForward(const FInputActionValue& Value)
+{
+	if (Controller != nullptr && Value.Get<float>() != 0.f)
+	{
+		// Getting a vector parallel to the ground but pointing in the direction of the Yaw of the controller
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.0f);
+		const FVector Direction( FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(Direction, Value.Get<float>());
+	}
+}
+
+void ABlasterCharacter::MoveRight(const FInputActionValue& Value)
+{
+	float FloatValue = Value.Get<float>();
+
+	if (Controller != nullptr && Value.Get<float>() != 0.f)
+	{
+		// Getting a vector parallel to the ground but pointing perpendicular to the direction of the Yaw of the controller
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.0f);
+		const FVector Direction( FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(Direction, Value.Get<float>());
+	}
+}
+
+void ABlasterCharacter::Turn(const FInputActionValue& Value)
+{
+	AddControllerYawInput(Value.Get<float>());
+}
+
+void ABlasterCharacter::LookUp(const FInputActionValue& Value)
+{
+	AddControllerPitchInput(Value.Get<float>());
+}
+
+
+
---
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "BlasterCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class BLASTER_API ABlasterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABlasterCharacter();
+	virtual void Tick(float DeltaTime) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input	)
+    UInputMappingContext* CharacterMappingContext;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input	)
+	UInputAction* MoveForwardAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input	)
+	UInputAction* MoveRightAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input	)
+	UInputAction* TurnAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input	)
+	UInputAction* LookUpAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input	)
+	UInputAction* JumpAction;
+
+	
+	void MoveForward(const FInputActionValue& Value);
+	void MoveRight(const FInputActionValue& Value);
+	void Turn(const FInputActionValue& Value);
+	void LookUp(const FInputActionValue& Value);
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	class USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	class UCameraComponent* FollowCamera;
+public:	
+
+
+};
